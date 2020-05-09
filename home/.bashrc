00ffc3a952ebeb0c89946dc7219b8f48e37e7f9b
opdo <pedrilidio@gmail.com>
2020-05-09 00:15:38 -0300

    home folder

---
+++ home/.bashrc
@@ -0,0 +1,9 @@
+export PS1="\e[38;5;221m\W -> \e[0m"
+alias ls='ls --color=auto'
+alias chrome-refresh-windows-size='cp ~/.config/google-chrome/Default/PreferencesBKP ~/.config/google-chrome/Default/Preferences'
+alias py='python -q'
+alias minecraft='java -jar ~/Mineshafter-launcher.jar'
+source /etc/profile.d/vte.sh
+# export LANG=pt_BR.UTF-8
+# export LC_CTYPE=pt.UTF-8
+# export LC_ALL=pt.UTF-8
--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
 echo "Firefox config files (./firefox/chrome) must be manally linked to the folder 'chrome' inside 'Profile Directory' shown at about:support tab (enter that in address bar). E.g. 'ln -s ~/dotfiles/firefox/chrome ~/.mozilla/firefox/s0k1wzah.default/'"
-ln -si ~/dotfiles/.vimrc ~/.vimrc
-ln -si ~/dotfiles/.Xresources ~/.Xresources
+
+for i in $(ls -A ~/dotfiles/home); do
+	echo "Linking '~/.$i'..."
+	ln -si ~/dotfiles/home/$i ~/$i
+done
 
 mkdir ~/.config
 
-for i in $(ls ~/dotfiles/.config); do
-	ln -sTi ~/dotfiles/.config/$i ~/.config/$i
+for i in $(ls ~/dotfiles/config); do
+	echo "Linking '~/.config/$i'..."
+	ln -sTi ~/dotfiles/config/$i ~/.config/$i
 done
 
+echo Done.
