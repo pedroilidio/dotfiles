2423c1f076d7db9130c52d971d418f03972e60c3
opdo <pedrilidio@gmail.com>
2020-05-08 23:44:42 -0300

    firefox

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+echo "Firefox config files (./firefox/chrome) must be manally linked to the folder 'chrome' inside 'Profile Directory' shown at about:support tab (enter that in address bar). E.g. 'ln -s ~/dotfiles/firefox/chrome ~/.mozilla/firefox/s0k1wzah.default/'"
 ln -si ~/dotfiles/.vimrc ~/.vimrc
 ln -si ~/dotfiles/.Xresources ~/.Xresources
 
