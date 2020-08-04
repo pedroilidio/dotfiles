7040c2586a173e83b4b2a663c16808eb47fdb2c1
opdo <pedrilidio@gmail.com>
2020-08-03 21:53:20 -0300

    qutebrowser

--- install.sh
+++ install.sh
@@ -1,15 +1,13 @@
-echo "Firefox config files (./firefox/chrome) must be manally linked to the folder 'chrome' inside 'Profile Directory' shown at about:support tab (enter that in address bar). E.g. 'ln -s ~/dotfiles/firefox/chrome ~/.mozilla/firefox/s0k1wzah.default/'"
-
-for i in $(ls -A ~/dotfiles/home); do
-	echo "Linking '~/.$i'..."
-	ln -si ~/dotfiles/home/$i ~/$i
+for i in $(ls -A home); do
+	echo "Linking '~/$i'..."
+	ln -sri home/$i ~/$i
 done
 
 mkdir ~/.config
 
-for i in $(ls ~/dotfiles/config); do
+for i in $(ls config); do
 	echo "Linking '~/.config/$i'..."
-	ln -sTi ~/dotfiles/config/$i ~/.config/$i
+	ln -srTi config/$i ~/.config/$i
 done
 
 echo Done.
